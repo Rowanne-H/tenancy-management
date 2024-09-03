981712648a4837e0b666e1020e82975b8554e1dd
YUERONG HUANG <rowanne.h@hotmail.com>
2024-09-03 21:44:30 +1000

    create a component for transactions

---
+++ client/src/components/Transactions.js
@@ -0,0 +1,21 @@
+import React from "react";
+import DisplayTable from "./DisplayTable";
+import { FIELD_MAPPINGS } from "./DataMappingFields";
+
+function Transactions({ transactions, deleteTransaction }) {
+  console.log(transactions)
+  
+  const fields = FIELD_MAPPINGS["transactions"];
+
+  return (
+    <DisplayTable
+      items={transactions}
+      deleteItem={deleteTransaction}
+      fields={fields}
+      defaultSortBy="created_at"
+      type="transactions"
+    />
+  );
+}
+
+export default Transactions;
