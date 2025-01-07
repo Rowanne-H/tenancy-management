87d882f49a54ddac361c0c4bcc38444ea970a635
YUERONG HUANG <rowanne.h@hotmail.com>
2025-01-07 22:37:02 +1100

    update database

--- server/migrations/versions/f0a22459e37c_create_tables.py
+++ server/migrations/versions/f0a22459e37c_create_tables.py
@@ -1,8 +1,8 @@
 """create tables
 
-Revision ID: 8600e4657441
+Revision ID: f0a22459e37c
 Revises: 
-Create Date: 2024-12-01 16:18:27.688123
+Create Date: 2025-01-07 22:36:08.029657
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8600e4657441'
+revision = 'f0a22459e37c'
 down_revision = None
 branch_labels = None
 depends_on = None
